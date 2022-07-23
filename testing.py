439ebc8409bcbcb67dc5129c9f33c4a2403336b4
AghababyanSG <aghababyan.sg@phystech.edu>
2022-07-23 14:04:26 +0400

    uploaded new files

--- testing.py
+++ testing.py
@@ -1,14 +1,14 @@
 import cv2
 
-img = cv2.imread('Textures/11.jpg', 0)
+img = cv2.imread('Textures/kendall.jpg', 0)
 
-upperBody_cascade = cv2.CascadeClassifier('Haar_Files/haar_upperbody.xml')
+upperBody_cascade = cv2.CascadeClassifier('Haar_Files/haar_fullbody.xml')
 
 arrUpperBody = upperBody_cascade.detectMultiScale(img)
 if arrUpperBody != ():
     for (x, y, w, h) in arrUpperBody:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    print('body found')
+        print('body found')
 
 cv2.imshow('image', cv2.resize(img, (1152, 822)))
 cv2.waitKey(0)
